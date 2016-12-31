355557a6157c8e9bdc1f61087380f7867da12c01
Rafal Pysz <kuchasz1@o2.pl>
2016-12-31 12:47:49 +0100

    removed unused references

--- src/Web/Core/App.php
+++ src/Web/Core/App.php
@@ -3,20 +3,12 @@
 namespace PS\Web\Core;
 
 use DI\ContainerBuilder;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\Driver\XmlDriver;
-use Doctrine\ORM\Tools\Setup;
-use Interop\Container\ContainerInterface;
 use PS\Data\Repositories\ArtistRepository;
 use PS\Data\Repositories\IArtistRepository;
-use PS\Twig\Extensions\AssetsExtension;
-use PS\Web\Config\Db;
 use PS\Web\Config\DoctrineFactory;
 use PS\Web\Config\TwigFactory;
 use Slim\Views\Twig;
-use Slim\Views\TwigExtension;
-use Twig_SimpleFunction;
 
 class App extends \DI\Bridge\Slim\App
 {
